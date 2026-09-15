c20b52425f2f7224e27734ce3208636c4219afd3
raihan-jishan <raihanjishan3242@gmail.com>
2026-09-15 12:45:46 +0600

    just push the main structure code not production version

--- app/admin/all-recipes/page.js
+++ app/admin/all-recipes/page.js
@@ -1,17 +1,9 @@
-import { FoodCard } from "@/components/ui/card";
-import DashboardLayout from "@/layout/admin";
-  
-export default function page() {
-  return (
-  <DashboardLayout>
-      <div className="w-full overflow-x-auto pb-4 scrollbar-hide">
-      <div className="flex gap-4 mt-5 min-w-max px-1">
-        <FoodCard />
-        <FoodCard />
-        <FoodCard />
-      </div>
-    </div>
-  </DashboardLayout>
-  );
-}
  
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
